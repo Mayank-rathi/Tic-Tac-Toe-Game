db2bb6d3510ca41254f704b97f6a93a23c9e6200
ADMINISTRATOR <Mayankrathi33@gmail.com>
2020-02-07 16:52:55 +0530

    ADDED:Resetting the board

--- TicTacToeGame.sh
+++ TicTacToeGame.sh
@@ -1,2 +1,20 @@
-#!/bin/bash -x
+#!/bin/bash 
 echo "Welcome To Tic Tac Toe Game"
+
+declare -a arrayBoard
+
+arrayBoard=( 1 2 3 4 5 6 7 8 9 )
+
+displayBoard(){
+echo "|===|===|===|"
+echo "| ${arrayBoard[0]} | ${arrayBoard[1]} | ${arrayBoard[2]} |"
+echo "|===|===|===|"
+echo "| ${arrayBoard[3]} | ${arrayBoard[4]} | ${arrayBoard[5]} |"
+echo "|===|===|===|"
+echo "| ${arrayBoard[6]} | ${arrayBoard[7]} | ${arrayBoard[8]} |"
+echo "|===|===|===|"
+}
+
+
+
+
