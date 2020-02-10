8c23b6f4a1333decbd1ed8322e4c6c80762568ab
ADMINISTRATOR <Mayankrathi33@gmail.com>
2020-02-10 12:39:58 +0530

    ADDED:Winning position or tie

--- TicTacToeGame.sh
+++ TicTacToeGame.sh
@@ -5,19 +5,19 @@ declare -a arrayBoard
 
 #Variable
 player=0
-
-arrayBoard=( 1 2 3 4 5 6 7 8 9 )
+count=0
+player_Position=0
+arrayBoard=( " " " " " " " " " " " " " " " " " " " " " " )
 
 function displayBoard(){
 echo "|===|===|===|"
-echo "| ${arrayBoard[0]} | ${arrayBoard[1]} | ${arrayBoard[2]} |"
+echo "| ${arrayBoard[1]} | ${arrayBoard[2]} | ${arrayBoard[3]} |"
 echo "|===|===|===|"
-echo "| ${arrayBoard[3]} | ${arrayBoard[4]} | ${arrayBoard[5]} |"
+echo "| ${arrayBoard[4]} | ${arrayBoard[5]} | ${arrayBoard[6]} |"
 echo "|===|===|===|"
-echo "| ${arrayBoard[6]} | ${arrayBoard[7]} | ${arrayBoard[8]} |"
+echo "| ${arrayBoard[7]} | ${arrayBoard[8]} | ${arrayBoard[9]} |"
 echo "|===|===|===|"
 }
-
 function assignedLetter(){
 	if [ $((RANDOM%2)) -eq 0 ]
 	then
@@ -35,6 +35,72 @@ function whoPlayFirst(){
 		echo "$player play first"
 	fi
 }
+function forRowOneChecking() {
+	for(( i=1;i<=9;i=$(($i+3))))
+	do
+		if [[ ${arrayBoard[$i]} == ${arrayBoard[$(($i+1))]} && ${arrayBoard[$(($i+1))]} == ${arrayBoard[$(($i+2))]} && ${arrayBoard[$i]} == $player ]]
+		then
+			echo "Win: $player"
+			exit
+		fi
+	done
+}
+function forcolumnChecking() {
+	for(( j=1;j<=9;j++))
+	do
+		if [[ ${arrayBoard[$j]} == ${arrayBoard[$(($j+3))]} && ${arrayBoard[$(($j+3))]} == ${arrayBoard[$(($j+6))]} && ${arrayBoard[$j]} == $player ]]
+		then
+			echo "Win: $player"
+			exit
+		fi
+	done
+}
+function forDiagonalChecking(){
+		k=1
+		if [[ ${arrayBoard[1]} == ${arrayBoard[5]} && ${arrayBoard[5]} == ${arrayBoard[9]} && ${arrayBoard[1]} == $player ]]
+		then
+			echo "Win: $player"
+			exit
+		elif [[ ${arrayBoard[3]} == ${arrayBoard[5]} && ${arrayBoard[7]} == ${arrayBoard[5]} && ${arrayBoard[3]} == $player ]]
+		then
+			echo "Win: $player"
+			exit
+		fi
+}
+
+function valueRangeOneToNine(){
+	read -p "Enter value 1-9 " player_Position
+	if [[ $player_Position -le 9 && $player_Position -ge 1 ]]
+	then
+		echo "User Enter value is:" $player_Position
+	else
+		echo "Value range is 1-9 try other value"
+	fi
+}
+function duplicateCheck(){
+	valueRangeOneToNine
+	if [[ ${arrayBoard[$player_Position]} == " " ]]
+	then
+		arrayBoard[$player_Position]=$player
+		((count++))
+		displayBoard
+	else
+		echo "Already occuipied,Enter another value"
+	fi
+}
+
+
+function userInputValue() {
+	while [[ $count -lt 9 ]]
+	do
+		duplicateCheck
+		forRowOneChecking
+		forcolumnChecking
+		forDiagonalChecking
+	done
+}
+
 assignedLetter
 whoPlayFirst
 displayBoard
+userInputValue
