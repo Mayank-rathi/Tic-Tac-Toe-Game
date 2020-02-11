90edea8f6bf36b2f9f0b0e7d172f028acd18ed92
ADMINISTRATOR <Mayankrathi33@gmail.com>
2020-02-11 09:55:31 +0530

    ADDED:Computer and playing with player

--- TicTacToeGame.sh
+++ TicTacToeGame.sh
@@ -1,14 +1,15 @@
-#!/bin/bash 
+#!/bin/bash
 echo "Welcome To Tic Tac Toe Game"
 
 declare -a arrayBoard
 
 #Variable
-player=0
+player=X
+computer=O
 count=0
 player_Position=0
 arrayBoard=( " " " " " " " " " " " " " " " " " " " " " " )
-
+user=0
 function displayBoard(){
 echo "|===|===|===|"
 echo "| ${arrayBoard[1]} | ${arrayBoard[2]} | ${arrayBoard[3]} |"
@@ -18,61 +19,50 @@ echo "|===|===|===|"
 echo "| ${arrayBoard[7]} | ${arrayBoard[8]} | ${arrayBoard[9]} |"
 echo "|===|===|===|"
 }
-function assignedLetter(){
-	if [ $((RANDOM%2)) -eq 0 ]
-	then
-		player=X
-	else
-		player=O
-	fi
-	echo "Player Assigned Letter is $player"
-}
-function whoPlayFirst(){
-	if [ $((RANDOM%2)) -eq 0 ]
-	then
-		echo "$player play first"
-	else
-		echo "$player play first"
-	fi
-}
+if(($((RANDOM%2))==0))
+then
+	flag=true
+else
+	flag=false
+fi
+
 function forRowOneChecking() {
-	for(( i=1;i<=9;i=$(($i+3))))
+	for(( i=1;i<=9;i+=3))
 	do
-		if [[ ${arrayBoard[$i]} == ${arrayBoard[$(($i+1))]} && ${arrayBoard[$(($i+1))]} == ${arrayBoard[$(($i+2))]} && ${arrayBoard[$i]} == $player ]]
+		if [[ ${arrayBoard[$i]} == ${arrayBoard[$(($i+1))]} && ${arrayBoard[$(($i+1))]} == ${arrayBoard[$(($i+2))]} && ${arrayBoard[$i]} == "$user" ]]
 		then
-			echo "Win: $player"
+			echo "Win"
 			exit
 		fi
 	done
 }
+
 function forcolumnChecking() {
 	for(( j=1;j<=9;j++))
 	do
-		if [[ ${arrayBoard[$j]} == ${arrayBoard[$(($j+3))]} && ${arrayBoard[$(($j+3))]} == ${arrayBoard[$(($j+6))]} && ${arrayBoard[$j]} == $player ]]
+		if [[ ${arrayBoard[$j]} == ${arrayBoard[$((j+3))]} && ${arrayBoard[$((j+3))]} == ${arrayBoard[$((j+6))]} && ${arrayBoard[$j]} == "$user" ]]
 		then
-			echo "Win: $player"
+			echo "Win"
 			exit
 		fi
 	done
 }
 function forDiagonalChecking(){
-		k=1
 		if [[ ${arrayBoard[1]} == ${arrayBoard[5]} && ${arrayBoard[5]} == ${arrayBoard[9]} && ${arrayBoard[1]} == $player ]]
 		then
-			echo "Win: $player"
+			echo "Win"
 			exit
 		elif [[ ${arrayBoard[3]} == ${arrayBoard[5]} && ${arrayBoard[7]} == ${arrayBoard[5]} && ${arrayBoard[3]} == $player ]]
 		then
-			echo "Win: $player"
+			echo "Win"
 			exit
 		fi
 }
 
 function valueRangeOneToNine(){
-	read -p "Enter value 1-9 " player_Position
 	if [[ $player_Position -le 9 && $player_Position -ge 1 ]]
 	then
-		echo "User Enter value is:" $player_Position
+		echo "$1 Enter value is:" $player_Position
 	else
 		echo "Value range is 1-9 try other value"
 	fi
@@ -81,26 +71,55 @@ function duplicateCheck(){
 	valueRangeOneToNine
 	if [[ ${arrayBoard[$player_Position]} == " " ]]
 	then
-		arrayBoard[$player_Position]=$player
-		((count++))
-		displayBoard
+		echo "Enter value is" $player_Position
+	
+	elif [[ $count == 9 ]]
+	then
+		echo "TIE"
+		exit
 	else
 		echo "Already occuipied,Enter another value"
+		changeTurn
+	fi
+}
+function changeTurn(){
+	if [[ $flag == true ]]
+	then
+		user=$player
+		echo "User playing "
+		read -p "Enter value 1-9 " player_Position
+		duplicateCheck user
+		arrayBoard[$player_Position]=$player
+		displayBoard
+		flag=false
+	else
+		user=$computer
+		echo "computer playing "
+		player_Position=$((RANDOM%9+1))
+		duplicateCheck computer
+		arrayBoard[$player_Position]=$computer
+		displayBoard
+		flag=true
 	fi
 }
 
 
 function userInputValue() {
-	while [[ $count -lt 9 ]]
+	while [[ $count -le 20 ]]
 	do
-		duplicateCheck
-		forRowOneChecking
-		forcolumnChecking
-		forDiagonalChecking
+		if [[ $count == 9 ]]
+   	then
+     		echo "Game is tie"
+     		exit
+		fi
+			echo "count is $count"
+			changeTurn
+			((count++))
+			echo "countwr is $count"	
+			forRowOneChecking
+			forcolumnChecking
+			forDiagonalChecking
 	done
 }
-
-assignedLetter
-whoPlayFirst
-displayBoard
 userInputValue
+
